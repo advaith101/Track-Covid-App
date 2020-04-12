f04ea2ae79f38e5ac1ad87c77ffd4d5325696eb4
asekharan7 <asekharan7@gatech.edu>
2020-04-12 00:30:11 -0400

    deploying

--- server.js
+++ server.js
@@ -58,6 +58,16 @@ app.use('/api/users', users);
 app.use('/api/absences', absences);
 app.use('/api/filter', filter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+
+
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`\nServer started on port ${port}`));
