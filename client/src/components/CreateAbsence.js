64f40ca0286da6e0194f4cc04c6ac9b11d4dfae0
asekharan7 <asekharan7@gatech.edu>
2020-04-13 06:04:00 -0400

    feat: added datepicker

--- client/src/components/CreateAbsence.js
+++ client/src/components/CreateAbsence.js
@@ -1,5 +1,21 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const CreateAbsence = (props) => {
@@ -27,18 +43,38 @@ const CreateAbsence = (props) => {
 
   return (
     <div>
-      <Button color="dark" style={{marginBottom: '2rem'}} onClick={toggle}>Create/Update Absence</Button>
+      <Button onClick = {toggle} type="submit" fullWidth variant="contained" color="default" style={{marginBottom: '2rem'}} onClick={toggle}>Create/Update Absence</Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}>New Absence</ModalHeader>
         <ModalBody>
             <Form>
                 <FormGroup>
                     <Label for="startDate">Start Date</Label>
-                    <Input type="name" name="name" id="startDate" placeholder="Enter Start Date (eg. '01/01/2020')" onChange={e => newAbsenceQuery.startDate = e.target.value}/>
+                    {/* <Input type="name" name="name" id="startDate" placeholder="Enter Start Date (eg. '01/01/2020')" onChange={e => newAbsenceQuery.startDate = e.target.value}/> */}
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="startDate"
+                      type="date"
+                      onChange={e => newAbsenceQuery.startDate = e.target.value}
+                      id="startDate"
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label for="endDate">End Date</Label>
-                    <Input type="name" name="name" id="endDate" placeholder="Enter End Date (eg. '12/20/2020')" onChange={e => newAbsenceQuery.endDate = e.target.value}/>
+                    {/* <Input type="name" name="name" id="endDate" placeholder="Enter End Date (eg. '12/20/2020')" onChange={e => newAbsenceQuery.endDate = e.target.value}/> */}
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      name="endDate"
+                      type="date"
+                      onChange={e => newAbsenceQuery.endDate = e.target.value}
+                      id="endDate"
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label for="reason">Select Reason</Label>
@@ -50,6 +86,20 @@ const CreateAbsence = (props) => {
                     <option>Other Quarantine - sick</option>
                     <option>School/Business Closure</option>
                     </Input>
+
+                    {/* <Select
+                    labelId="reason"
+                    fullWidth
+                    id="reason"
+                    onChange={e => newAbsenceQuery.reason = e.target.value}
+                    >
+                    <MenuItem>--</MenuItem>
+                    <MenuItem>EE Quarantine - not sick</MenuItem>
+                    <MenuItem>EE Quarantine - sick</MenuItem>
+                    <MenuItem>Other Quarantine - not sick</MenuItem>
+                    <MenuItem>Other Quarantine - sick</MenuItem>
+                    <MenuItem>School/Business Closure</MenuItem>
+                    </Select> */}
                 </FormGroup>
             </Form>
         </ModalBody>
